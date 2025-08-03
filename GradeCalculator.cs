9c2e6a366be9ce5f8c2c008edd9bcffc42a7a11f
EdiscoKes <eskoranteng003@st.ug.edu.gh>
2025-08-03 21:13:49 +0000

    first commit

---
+++ GradeCalculator.cs
@@ -0,0 +1,14 @@
+using System;
+public class GradeCalculator
+{
+    public void Run()
+    {
+        Console.WriteLine("Enter your grade(0-100): ");
+        if (int.TryParse(Console.ReadLine(), out int grade))
+        {
+            if (grade >= 90 && grade <= 100)
+                Console.WriteLine("Grade: A");
+            
+        }
+    }
+} 
